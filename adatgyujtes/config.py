7f968e0f6e3c3f9e7564f5004ea5ba1bf92951dc
Villányi Bálint <vilanyib@gmail.com>
2019-05-06 15:43:01 +0200

    Added ID registration, incomplete

--- adatgyujtes/config.py
+++ adatgyujtes/config.py
@@ -1,4 +1,8 @@
 import configparser
+import random
+import string
+import requests
+import pdb
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -19,5 +23,33 @@ CONFIG_TEST = {
     'PASSWORD': config['DEFAULT']['PASSWORD']
 }
 
-def register_id():
-    pass
+def configure(credential):
+    gen_id = random_string()
+    result = requests.post(url=config['REGISTER']['REGISTER_URL'], json={"generationCredential": credential, "id": gen_id})
+    if result.ok:
+        if result.json()['id'] == gen_id:
+            config.set('DEFAULT', 'ADATGYUJTES_ID', gen_id)
+
+            password = random_string(8)
+            config.set('DEFAULT', 'PASSWORD', password)
+
+            try:
+                import privadome
+                # TODO
+                config.set('DEFAULT', 'DATABASE_PATH', privadome.__file__)
+            except ImportError as e:
+                print(e)
+
+            config.write(open('config.ini', 'w'))
+            print('Eszköz sikeresen regisztrálva.')
+            print('Jelszó: ', password)
+        else:
+            print('Nem sikerült regisztrálni az eszközt.', result.content)
+    else:
+        print('Nem sikerült regisztrálni az eszközt.', result.content)
+        pdb.set_trace()
+            
+
+def random_string(length = 16):
+    letters = string.ascii_letters + string.digits
+    return ''.join(random.choice(letters) for i in range(length))
